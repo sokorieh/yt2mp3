476ac3c9c3506669bc44a96b5db1893823430ff5
sokorieh <ericericsouza02@gmail.com>
2023-01-01 04:11:16 -0500

    added menu and choice of playlist or song

--- main.py
+++ main.py
@@ -4,11 +4,25 @@ import song
 from pytube import YouTube, Playlist
 from art import *
 
+# simple greet function using ascii art
 def greet():
     tprint("yt2mp3")
     print ('-' * 44)
+    print()
 
+def menu():
+    print('[1] for a playlist download')
+    print('[2] for an audio download from a video')
+    print()
 
+def getInput():
+    while True:
+        ask = int(input('Enter either 1 or 2: '))
 
+        if ask == 1 or ask == 2:
+            return ask
+    
 
 greet()
+menu()
+method = getInput()
